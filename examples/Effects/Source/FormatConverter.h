19d27ef72ad8971c5798f3e789dca64d952d444f
Matt Gonzalez <matt@echoaudio.com>
2024-07-06 04:05:06 -0700

    Rewrote FormatConverter to match software renderer

--- examples/Effects/Source/FormatConverter.h
+++ examples/Effects/Source/FormatConverter.h
@@ -10,20 +10,7 @@ public:
 
     juce::Image convert(const juce::Image& source, juce::Image::PixelFormat outputFormat);
 
-    void print(juce::Image const& image)
-    {
-        juce::Image::BitmapData bitmapData{ image, juce::Image::BitmapData::readOnly };
-        for (int y = 0; y < image.getHeight(); ++y)
-        {
-            juce::String line;
-            for (int x = 0; x < image.getWidth(); ++x)
-            {
-                line << bitmapData.getPixelColour(x, y).toString().paddedLeft('0', 8) << " ";
-            }
-
-            DBG(line);
-        }
-    }
+    void print(juce::Image const& image);
 
 private:
     juce::Image convertToSingleChannel(const juce::Image& source);
