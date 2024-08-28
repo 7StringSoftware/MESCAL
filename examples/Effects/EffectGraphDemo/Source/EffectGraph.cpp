3b68a8d0f3e69e9237dd9e1a7bc0b63ed7dfb51c
Matt Gonzalez <matt@echoaudio.com>
2024-08-27 21:09:23 -0700

    Docs

--- examples/Effects/EffectGraphDemo/Source/EffectGraph.cpp
+++ examples/Effects/EffectGraphDemo/Source/EffectGraph.cpp
@@ -2,6 +2,34 @@
 
 EffectGraph::EffectGraph()
 {
+	/*
+	
+		Build the effect graph demonstration
+
+		This graph will take an image drawn with simple geometric objects (rectangles, stars, etc), and add shadow and lighting effects
+		to create a pseudo-3D metallic knob.
+
+        The final image will be a composite: the lighting effect output is layered on top of the original image, which in turn is layered on top
+        the output of the shadow effect.
+
+		Each layer in the composite is painted by a different chain of effects which are ultimately combined by compositing effects.
+
+		To create the shadow, the source image is fed into a shadow effect. The shadow effect is then chained to a 2D affine transform effect 
+		to shift the shadow down and to the right.
+
+		To create the lighting effect, the source image is fed into a Gaussian blur effect and then a spot specular lighting effect. The output 
+		of the spot specular lighting effect is fed into a composite effect. 
+
+		To further accentuate the metallic look, the output of that composite effect is combined with the original source image using an arithmetic
+		composite effect.
+
+		The final image is created by a composite effect that combines the	output of the 2D affine transform and the output of the arithmetic
+		composite effect.
+
+		Note that there's no need to retain any of the upstream effects; you only need to keep the final output effect. Effects are reference-counted,
+		so the entire graph will be retained until no longer needed.
+	
+	*/
 	auto blurEffect = new mescal::Effect{ mescal::Effect::Type::gaussianBlur };
 	blurEffect->setPropertyValue(mescal::Effect::GaussianBlur::standardDeviation, 10.0f);
 	blurEffect->setInput(0, sourceImage);
