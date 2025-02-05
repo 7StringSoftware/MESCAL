e83f91a114c730986ad5754d901b4c50212b7742
Matt Gonzalez <matt@echoaudio.com>
2025-02-04 16:40:11 -0800

    Update to match JUCE develop

--- module/mescal/mescal.cpp
+++ module/mescal/mescal.cpp
@@ -34,6 +34,7 @@
 #include <juce_graphics/native/juce_Direct2DMetrics_windows.h>
 #include <juce_graphics/native/juce_EventTracing.h>
 #include <juce_graphics/native/juce_DirectX_windows.h>
+#include <juce_graphics/native/juce_Direct2DPixelDataPage_windows.h>
 #include <juce_graphics/native/juce_Direct2DImage_windows.h>
 #include <juce_graphics/native/juce_Direct2DGraphicsContext_windows.h>
 #include <juce_graphics/native/juce_Direct2DImageContext_windows.h>
